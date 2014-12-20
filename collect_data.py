c0d6e7583d336c7d7cf7d9fba3fd82f3f46b56c7
Clarissa Garvey <ccg1731@gmail.com>
2014-12-19 23:11:47 -0500

    Add: general election data collection, printing
    Added general data collection about the election, such as when
    the election happened, and if the election was a tie. Currently,
    the data gets printed to standard out after being formatted nicely
    as a JSON object.
    
    Several functions were added: postListeners, tallyVotes,
    averageVotes, checkTied, checkRandom, and requestInfo.
    
    The individual song information is currently not included, and
    prints out as an empty array.

--- collect_data.py
+++ collect_data.py
@@ -16,36 +16,65 @@ def main():
     # General constant values
     sid_dict = { 'OCR' : 2 , 'ALL' : 5 }
     
-    # Get information
-    response = getInfo(my_id, my_key, sid_dict['ALL'])
+    # Retrieve listeners information (it's not given in sync)
+    listeners_response = postListeners(my_id, my_key, sid_dict['OCR'])
+    listeners = json.loads(listeners_response.read().decode('utf-8'))
+    
+    # Retrieve station information
+    sid = sid_dict['OCR']
+    response = getInfo(my_id, my_key, sid)
     data = json.loads(response.read().decode('utf-8'))
-    
-    #Pull out important bits
+
+    # Pull out important bits
     current = data['sched_current']
+    previous = data['sched_history']
     
+    '''
+    this does not work; voting is never allowed on current.
+    Maybe check with a while loop?
     # It's useless to take stats when voting's not allowed.
     if not current['voting_allowed']:
+        print('No voting.')
         return
         #contine #in the future, we will want to just skip this iteration
-
-    #TODO: remove the information for printing.
+    '''
+    epoch_time = current['start']
+    n_songs = len(current['songs'])
+    prev_votes = tallyVotes(previous[0])
     
+    voter_avg = averageVotes(previous)
+    prev_winner_rating = previous[0]['songs'][0]['rating']
+    is_random = checkRandom(current)
     
     '''
-    Print info about station, time, who listeners, previous election's number of
-    voters, number of songs in the election, voter average over the past 10 min,
-    and previous election winner's average rating, and if election was a tie*
+    Get info about station, time, listeners, previous election's number of
+    voters, number of songs in the election, voter average over past 5 songs,
+    previous election winner's average rating, and if election was a tie*
     *without a request in the tie(??)
     '''
+    election_out = { 'station' : sid,
+                     'time' : epoch_time, #subject to change!
+                     'number_songs' : n_songs,
+                     'prev_elec_votes' : prev_votes,
+                     'prev_5_elec_vote_avg' : voter_avg,
+                     'prev_winner_rating' : prev_winner_rating,
+                     'chosen_randomly' : is_random,
+                     'listeners' : listeners,
+                     'songs' : [] }
     
     '''
-    Print info for each song: title, cooldown group, album, average rating,
-    average rating for its album, request (0 or 1), requester's name,
-    origin SID (if applicable), length, winner or loser, did it tie with winner?
+    Get info for each song: title, cooldown group, album, average rating,
+    average rating for its album, was it a request or not, requester's name,
+    origin SID (if applicable), length, did it play, did it tie with winner?,
+    artists
     All 'string-like' things should be string + IDs when possible/reasonable.
     '''
+    # get information that can be fetched from a single song
+    for song in current['songs']:
+        election_out['songs'].append(processSong(song))
+    # TODO: did it tie with the winner + did it play?
     
-    print(data)
+    print(json.dumps(election_out, indent=4, sort_keys=True))
 
 def getInfo(my_id, my_key, station):
     """
@@ -84,7 +113,7 @@ def postSync(my_id, my_key, station):
     #Params for syncing
     params = { 'user_id' : my_id,
                'key' : my_key,
-               'sid' : station}
+               'sid' : station }
     data = urlparse.urlencode(params)
     data = data.encode('utf-8')
     
@@ -92,5 +121,119 @@ def postSync(my_id, my_key, station):
     sync_url = 'http://rainwave.cc/api4/sync'
     return urlrequest.urlopen(sync_url, data)
 
+def postListeners(my_id, my_key, station):
+    '''
+    Perform a POST call to /api/current_listeners.
+    
+    POST to /api/sync doesn't include this information.
+
+    param: my_id,   a positive integer ID for the user
+    param: my_key,  a string API key for the user
+    param: station, a positive integer indicating the station (1-5)
+    return:         the raw response (bytestream)  
+    '''
+    params = { 'user_id' : my_id,
+               'key' : my_key,
+               'sid' : station }
+    data = urlparse.urlencode(params)
+    data = data.encode('utf-8')
+    
+    #Perform sync
+    listeners_url = 'http://rainwave.cc/api4/current_listeners'
+    return urlrequest.urlopen(listeners_url, data)
+
+def tallyVotes(election):
+    '''
+    Tallies the votes in an election.
+    
+    param: election,  a dictionary, in standard rainwave format, of an election
+    return:           an integer count of the number of total votes
+    '''
+    songs = election['songs']
+    total = 0
+    
+    for song in songs:
+        total += song['entry_votes']
+    
+    return total
+
+def averageVotes(elections):
+    '''
+    Averages the votes over all elections, presumably in previous elections.
+    
+    param: elections,  a dictionary containing several elections
+    return:            a double number of votes, the average over all
+    '''
+    total = 0
+    count = 0
+    
+    for entry in elections:
+        for song in entry['songs']:
+            total += song['entry_votes']
+        count += 1
+    
+    return total/count
+
+def checkTied(election):
+    '''
+    Determines if an election was a tie.
+    
+    param: election, the election to check (in standard rainwave format)
+    return: True iff. the election was tied, regardless of requests
+    return: number of songs tied
+    '''
+    songs = election['songs']
+    
+    # Single-song elections can't tie.
+    if len(songs) <= 1:
+        return false
+    
+    # Election is only tied if the top two songs have the same number of votes.
+    n_tied = 1
+    for i in range(0, len(songs)-1):
+        if songs[i]['entry_votes'] == songs[i+1]['entry_votes']:
+            n_tied += 1
+        else:
+            break
+    
+    return (n_tied > 1), n_tied
+    
+def checkRandom(election):
+    '''
+    Determines if the result of an election was chosen randomly.
+    
+    param: election, the election to check (in standard rainwave format)
+    return:          True iff. the election was randomly chosen.
+    '''
+    songs = election['songs']
+    
+    # Check if there was a tie at all. No tie -> not random.
+    # Also handles single-song elections. 
+    is_tied = checkTied(election)[0]
+    
+    if not is_tied:
+        return False
+    
+    # If the winner was a request, and the second wasn't, it wasn't random.
+    # (handles special case: 
+    elif requestInfo(songs[0])[0] and not requestInfo(songs[1])[0]:
+        return False
+    else:
+        return True
+
+def requestInfo(song):
+    '''
+    Returns information about whether or not a request was made on a song, and,
+    if so, who requested it.
+    
+    param: song,  the song object (in standard rainwave foramt)
+    return:       True iff. the song was requested, False otherwise
+    return:       the name of the requester, or None if there was no request
+    '''
+    name = song['elec_request_username']
+    if name == None:
+        return False, None
+    else:
+        return True, name
 
 main()
